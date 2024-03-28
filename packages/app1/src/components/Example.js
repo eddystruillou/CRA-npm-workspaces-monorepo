dd7099f5343a4a2788bb3536fdab2c9244654416
Eddy Struillou <34336084+eddystruillou@users.noreply.github.com>
2024-03-28 16:43:32 +0100

    Create example component

---
+++ packages/app1/src/components/Example.js
@@ -0,0 +1,12 @@
+const Example = () => {
+  return (
+    <section>
+      <h1>Example component</h1>
+      <hr />
+        Toggle message
+      <hr />
+    </section>
+  );
+}
+
+export default Example;
