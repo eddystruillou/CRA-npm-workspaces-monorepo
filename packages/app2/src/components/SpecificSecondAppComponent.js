d9ad94d3f40b12727e04146180571370e095b204
Eddy Struillou <34336084+eddystruillou@users.noreply.github.com>
2024-03-28 17:23:44 +0100

    Update common project and create shared button

--- packages/app2/src/components/SpecificSecondAppComponent.js
+++ packages/app2/src/components/SpecificSecondAppComponent.js
@@ -1,8 +1,11 @@
+import { Button } from '@backoffice/common';
+
 const SpecificSecondAppComponent = () => {
   return (
     <section>
       <h1>Specific App 2 Component</h1>
       <hr />
+      <Button />
         Toggle message for App 2
       <hr />
     </section>
