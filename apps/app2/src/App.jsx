d18494d8060ff63a2d67eb957c86c379cf16cd03
Eddy Struillou <34336084+eddystruillou@users.noreply.github.com>
2024-04-04 17:33:07 +0200

    Use math-helper librairy in vite app2

--- apps/app2/src/App.jsx
+++ apps/app2/src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { add } from 'math-helpers'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  
+  useEffect(() => {
+    console.log(add(1, 2));
+  }, []);
 
   return (
     <>
