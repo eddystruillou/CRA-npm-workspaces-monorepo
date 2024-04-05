34ebc4334f7abb376aa56f0cb69db96775508ebd
Eddy Struillou <34336084+eddystruillou@users.noreply.github.com>
2024-04-05 14:49:17 +0200

    Export lib and update vite.config.js to do this

--- packages/backoffice-components/vite.config.js
+++ packages/backoffice-components/vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+
+// What that file does is it uses react plugin, and instructs Vite to use lib/main.js as an entry point,
+// name the file, and library just library, and externalize react and react-dom dependencies.
+// Meaning these dependencies won’t be bundled into your library, rather, they will be required to be installed by the project that uses that library.
+//So the library build will be smaller.
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.js"),
+      name: "library",
+      fileName: "library",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "React-dom",
+        },
+      },
+    },
+  },
 })
