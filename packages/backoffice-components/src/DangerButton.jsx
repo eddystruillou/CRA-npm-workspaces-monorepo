7ff3ee33cea8d475608d9333fad628f7558fd3b9
Eddy Struillou <34336084+eddystruillou@users.noreply.github.com>
2024-04-09 15:52:32 +0200

    Use turbo on main package.json

--- packages/backoffice-components/src/DangerButton.jsx
+++ packages/backoffice-components/src/DangerButton.jsx
@@ -2,6 +2,6 @@ import { Button } from 'antd';
 
 export default function DangerButton() {
     return (
-        <Button danger>Dangeron</Button>
+        <Button danger>Danger button</Button>
     )
 }
